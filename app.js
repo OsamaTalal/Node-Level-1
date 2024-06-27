dd1654602fe66078f7c9bb47a534cc9cbb56d319
OsamaTalal <169051670+OsamaTalal@users.noreply.github.com>
2024-06-27 14:15:29 +0300

    Update 2

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+app.use(express.urlencoded({ extended: true }))
+const Mydata = require("./models/mydataSchema")
+
+
+
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
+  res.sendFile("./views/home.html", { root: __dirname })
+})
+
+
+app.get('/index.html', (req, res) => {
+  res.send("<h1> The Name added successfully </h1>")
 })
 
 
 mongoose.connect("mongodb+srv://osamatalal744:o123@cluster0.d2wvcro.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  app.listen(port, () => {
-    console.log(`http://localhost:${port}/`)
-  });
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`)
+    });
+  })
+  .catch((err) => { console.log(err) });
+
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+
+  const mydata = new Mydata(req.body);
+  mydata.save().then(() => {
+    res.redirect("/index.html")
+  }).catch((err) =>{
+    console.log(err)
+  })
+
 })
-.catch((err) => {console.log(err)});
