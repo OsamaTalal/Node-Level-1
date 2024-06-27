2104077138f3d6b3e05dcb682cf7fc1ccd77e630
OsamaTalal <169051670+OsamaTalal@users.noreply.github.com>
2024-06-27 12:30:26 +0300

    update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html", {root: __dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+
+mongoose.connect("mongodb+srv://osamatalal744:o123@cluster0.d2wvcro.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
+  });
 })
+.catch((err) => {console.log(err)});
